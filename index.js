e7254b08edb9d5bf2dd2ca1162a1316cb0735930
Prathibha <pratibha.indel@gmail.com>
2022-02-25 06:11:38 -0500

    updated validation to file

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
 //Declaring dependencies and variables
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require("inquirer");//provides functionality to prompt question
+const fs = require("fs");//interacting with the file system
 
 function promptUser() {
-
-  return inquirer.prompt([
+//provides functionality to prompt function
+ //project title 
+return inquirer.prompt([
     {
       type: "input",
       name: "ProjectTitle",
-      message: "what is the Project title?"
-
+      message: "what is the Project title?",
+      validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('You need to enter a title to continue!');
+            return false;
+        }
+    }
     },
+  // project description  
     {
       type: "input",
       name: "Description",
-      message: "Write brief description of  the Project:"
-
+      message: "Write brief description of  the Project:",
+      validate: descriptionInput => {
+       if (descriptionInput) {
+           return true;
+        } else {
+             console.log('You need to enter a  project description!');
+             return false;
+        }
+    }
     },
-
+    // project installation
     {
       type: "input",
       name: "Installation",
-      message: "Write about installation if any: "
-
-    },
+      message: "Write about installation if any: ",
+      validate: installationInput => {
+        if (installationInput) {
+            return true;
+         } else {
+              console.log('please enter about installation process!');
+              return false;
+         }
+     }
+     },
+     // project usage
     {
       type: "input",
       name: "Usage",
-      message: "what is this project usage for?"
-
-    },
+      message: "what is this project usage for?",
+      validate: usageInput => {
+        if (usageInput) {
+            return true;
+         } else {
+              console.log('You need to enter about usage of project!');
+              return false;
+         }
+     }
+     },
+     // License 
     {
       type: "list",
       name: "License",
@@ -42,32 +74,55 @@ function promptUser() {
         "MIT",
         "Mozilla",
         "Open"
-      ]
-
-    },
+      ]      
+     },
+     //project contributors
     {
       type: "input",
       name: "Contributors",
-      message: "who are the contributers of this project?"
-
-
-    },
+      message: "who are the contributers of this project?",
+      validate: contInput => {
+        if (contInput) {
+            return true;
+         } else {
+              console.log('You need to enter about  project contributors!');
+              return false;
+         }
+     }
+      },
+      //Test
     {
       type: "input",
       name: "Test",
       message: "is there a test included?"
-
     },
+    //github username
     {
       type: "input",
       name: "username",
-      message: "please enter the github username: "
-
+      message: "please enter the github username: ",
+      validate: usernameInput => {
+        if (usernameInput) {
+            return true;
+         } else {
+              console.log('You need to enter a  github username!');
+              return false;
+         }
+     }
     },
+    //E-mail
     {
       type: "input",
       name: "email",
-      message: "please enter your email: "
+      message: "please enter your email: ",
+      validate: emailInput => {
+        if (emailInput) {
+            return true;
+         } else {
+              console.log('please enter your e-mail');
+              return false;
+         }
+     }
 
     }
 
@@ -76,7 +131,8 @@ function promptUser() {
 
 function generateReadme(answer) {
   return `# ${answer.ProjectTitle}
-  ## license 
+  
+  ## License 
   [![license](https://img.shields.io/badge/license-${answer.License}-blue)](https://shields.io)
 
   #### Table of Contents
@@ -116,12 +172,9 @@ function generateReadme(answer) {
    ## Questions
    * For additional help or questions about collaboration, please reach out to ${answer.email}
    * Follow me on Github at [${answer.username}](http://github.com/${answer.username})`;
-
-
-
-
 }
 
+// function to initialize app and write file
 function init() {
   promptUser()
     .then(function (answers) {
@@ -137,6 +190,7 @@ function init() {
     });
 
 }
+//function call to initialise 
 init();
 
 
